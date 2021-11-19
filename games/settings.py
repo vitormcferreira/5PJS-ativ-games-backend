d50fe2062dc0f1a72e9961e7e1217ae69ebff67c
Vitor <vitorpgmdr@gmail.com>
2021-11-19 17:43:00 -0300

    configurar django-cors-headers

--- games/settings.py
+++ games/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # https://www.django-rest-framework.org/tutorial/quickstart/
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +134,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+# https://pypi.org/project/django-cors-headers/
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
