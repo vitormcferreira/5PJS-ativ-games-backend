360724e56dd1f94d80fd16b9bd26aa44d4490d2b
Vitor <vitorpgmdr@gmail.com>
2021-11-22 18:03:57 -0300

    corrigir url

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('jogo_da_memoria/', include('jogo_da_memoria.urls')),
-    path('token/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
