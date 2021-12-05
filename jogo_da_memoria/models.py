04749ff18ba56136dd183247915116e5f8cff242
Vitor <>
2021-12-05 03:22:11 -0300

    editar faz_movimento()

--- jogo_da_memoria/models.py
+++ jogo_da_memoria/models.py
@@ -33,17 +33,43 @@ class JogoDaMemoria(models.Model):
         return instance
 
     def faz_movimento(self, h1, h2):
+        """
+        Recebe h1 e h2, que são os numeros das cartas.
+
+        Se for um par correto, seta os valores do par em self.parsed_cartas, 
+        apaga o par de self.cartas, marca o par como encontrado, incrementa 
+        self.acertos e salva as alterações na base de dados.
+
+        Caso for um par errado, seta os valores do par em self.parsed_cartas e 
+        retorna.
+        """
         self.jogadas += 1
 
         carta1 = self.cartas.get(h1)
         carta2 = self.cartas.get(h2)
 
-        if carta1 == carta2 and carta1 != None and carta2 != None:
+        pos_carta1 = None
+        pos_carta2 = None
+
+        for carta in self.parsed_cartas:
+            if carta[0] == h1:
+                carta[1] = carta1
+                pos_carta1 = carta
+            elif carta[0] == h2:
+                carta[1] = carta2
+                pos_carta2 = carta
+
+        if carta1 == carta2 and carta1 != None and carta2 != None and h1 != h2:
             self.acertos += 1
+
+            pos_carta1[2] = True
+            pos_carta2[2] = True
+
             del self.cartas[h1]
             del self.cartas[h2]
 
-        self.save()
+            self.save()
+
         return carta1, carta2
 
     @classmethod
