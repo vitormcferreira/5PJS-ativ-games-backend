0eca378229665ad8daa4ad1d19bb3ea7d5c17ad8
Vitor <>
2021-12-18 20:32:13 -0300

    corrigir validação

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from .models import Usuario
 
@@ -10,7 +9,8 @@ class UsuarioSerializer(serializers.ModelSerializer):
         model = Usuario
         fields = ['id', 'username', 'password']
         # https://www.django-rest-framework.org/api-guide/serializers/#additional-keyword-arguments
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True,
+                                     'validators': [validate_password]}}
 
     def create(self, validated_data):
         user = Usuario(username=validated_data['username'])
@@ -18,12 +18,3 @@ class UsuarioSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-
-    # https://www.django-rest-framework.org/api-guide/serializers/#validation
-    def validate_password(self, value):
-        try:
-            validate_password(value)
-        except ValidationError as err:
-            raise ValidationError
-
-        return value
